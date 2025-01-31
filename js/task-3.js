1901919c292a65ba208d56ffc7ec727e132ac550
Julia Jaworska <ulia012008@gmail.com>
2025-01-31 21:04:58 +0100

    home work

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,22 @@
-function checkForSpam(message) {
-    const lowercaseMessage = message.toLowerCase();
-      return lowercaseMessage.includes('spam') || lowercaseMessage.includes('sale');
-}  
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
